3b30bc0a090d8bd4a5bdb37eb68999c721da2ef1
NOONE <s4cxh4@gmail.com>
2025-02-12 11:00:14 +0100

    Update main.py

--- XRay-Tools-main/Xray-Tools - Mobile/main.py
+++ XRay-Tools-main/Xray-Tools - Mobile/main.py
@@ -716,8 +716,8 @@ BOT_TOKEN = '{bot_token}'\n""")
 {RED}[USER : {RST}{os.getlogin()}{RED}]               
 {RED}[WORKING ON : {RST}{os_info}{RED}]              
 {RED}[USER IP : {RST}{user_ip}{RED}]
-{RED}[DISCORD :{RST} https://discord.gg/CfRTmZqJAV{RED}]
-{RED}[GITHUB :{RST} https://github.com/k4l4shnik0pyw{RED}]
+{RED}[DISCORD :{RST} https://discord.gg/9274AD29fA{RED}]
+{RED}[GITHUB :{RST} https://github.com/NoonePYDEV{RED}]
 {RED}[BY {RST}K4L4SHNIK0V{RED}]                  
     """
 
